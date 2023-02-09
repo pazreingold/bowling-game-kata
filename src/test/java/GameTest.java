900e9cfe9e731d051d18ffcbe7a6a3984533995e
pazrein <pazreingold1996@gmail.com>
2023-02-09 23:47:02 +0200

    Refactor - Initialize Game BeforeEach

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,23 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void ScoreIs0WhenGameStarts() {
-        assertEquals(0, new Game().score());
+        assertEquals(0, game.score());
     }
 
     @Test
     void ScoreIs1WhenRoll1() {
-        Game game = new Game();
         game.roll(1);
         assertEquals(1, game.score());
     }
 
     @Test
     void ScoreIs2WhenRoll1And1() {
-        Game game = new Game();
         game.roll(1);
         game.roll(1);
         assertEquals(2, game.score());
